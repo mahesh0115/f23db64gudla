6beb0855aa78750b461ee26961ee43c6f13941bd
mahesh0115 <133246966+mahesh0115@users.noreply.github.com>
2023-11-01 22:13:27 -0500

    Grid board computed

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dumbellsRouter = require('./routes/dumbells');
+var boardRouter = require('./routes/board');
 
 
 var app = express();
@@ -24,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dumbells', dumbellsRouter);
+app.use('/board', boardRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
